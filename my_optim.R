6caf5b7c7ec1d0c7b06e48adf51f46c24e9abbda
jorub <reintheoretisch@gmail.com>
2018-12-04 15:02:26 +0100

    fixed my_optim, added more options

--- my_optim.R
+++ my_optim.R
@@ -1,14 +1,19 @@
-my_optim <- function(ini_vals){
+my_optim <- function(ini_vals, alpha, beta, gamma, maxit, abstol){
   source("drut_eval.R")
   out <- optim( 
     par = ini_vals,
     fn = eval_fun_mead,
     control = list(
-      alpha = 0.01,
-      beta = 0.005,
-      gamma = 0.02
+      alpha = alpha,
+      beta = beta,
+      gamma = gamma,
+      maxit = maxit,
+      abstol = abstol
     )
   )
   print(out)
-  write(c(out$par, out$value, out$convergence), "results/results.out")
+  if(out$convergence != 0){
+    print("something is fucked up!!!!!!")
+  }
+  write(c(out$par, out$value, out$counts[1], out$convergence), "results/results.out", )
 }
